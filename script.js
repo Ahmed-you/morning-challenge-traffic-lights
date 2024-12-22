2eef8494574774b4d7a8ac50900440db4e08611b
MonkeyDluffySan <wwwwwahmedyounis@gmail.com>
2024-12-22 18:51:19 +0200

    just-part-1-me-done

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // DON'T CHANGE ANYTHING BETWEEN HERE ---
 var time = 1000;
 
-var red = blinkLight(get('light-top'), 'red');
-var yellow = blinkLight(get('light-middle'), 'yellow');
-var green = blinkLight(get('light-bottom'), 'green');
+var red = blinkLight(get("light-top"), "red");
+var yellow = blinkLight(get("light-middle"), "yellow");
+var green = blinkLight(get("light-bottom"), "green");
 
-var red2 = blinkLight(get('light-top2'), 'red');
-var yellow2 = blinkLight(get('light-middle2'), 'yellow');
-var green2 = blinkLight(get('light-bottom2'), 'green');
+var red2 = blinkLight(get("light-top2"), "red");
+var yellow2 = blinkLight(get("light-middle2"), "yellow");
+var green2 = blinkLight(get("light-bottom2"), "green");
 
 // blinkLight returns a function that switches a light on then waits a second
 // before switching it off again and calling its first argument as a 'callback'.
 // If no arguments are supplied the light just goes off.
 function blinkLight(element, colour) {
-  var blink = function(callback) {
+  var blink = function (callback) {
     element.classList.add(colour);
-    setTimeout(function() {
+    setTimeout(function () {
       element.classList.remove(colour);
       if (callback) {
         callback();
@@ -31,17 +31,17 @@ function get(id) {
 }
 
 // --- AND HERE.
-// Replace the example code below with your own solution:
 
-function light() {
-  // HINT: when red() finishes it calls the first argument as a function.
-  // So in this example the flow is red() -> wait a second -> green()
-  red(green);
+function light(callback) {
+ 
+  // i mean ... it looks weird in arrow functions but i did it Meh..
+  green(() =>
+    yellow(() =>
+      red(() =>
+        red(() => red(() => red(yellow(() => green(callback(callback))))))
+      )
+    )
+  );
 }
-light();
+light(light);
 
-function light2() {
-  // Same as above except the flow is green2() -> wait a second -> red2()
-  green2(red2);
-}
-light2();
